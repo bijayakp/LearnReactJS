b58d2160805b034b6c8ee077c7a1d6f73cd1b1ab
Bijaya Kumar. Pathia <BijayaP@edynamic.net>
2022-05-06 12:42:18 +0530

    Addition the assignments

--- src/DigitalClock.js
+++ src/DigitalClock.js
@@ -6,27 +6,28 @@ class DigitalClock extends React.Component {
         this.state = {
             currentDate : new Date()
         }
-        this.MountClock();
+       
     }
 
    
 
-    MountClock = () => {
+    componentDidMount = () => {
         this.timer = setInterval(
             () => this.setState({currentDate: new Date()}),
             1000
         );
     }
 
-    UnmountClock = () => {
+    componentWillUnmount = () => {
         clearInterval(this.timer);
     }
 
     render() {
         return(
             <>
-                <h2>Digital Clock</h2>
-                <div className="clock-face">
+                <h2 className='digitalClock-Title'>Digital Clock</h2>
+                <hr></hr>
+                <div className="digitalClock-Clock">
                     {this.state.currentDate.toLocaleString()}
                 </div>
             </>
